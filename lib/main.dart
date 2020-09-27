e732b97dd2ae962278a383d313e68829997b51ca
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2020-09-27 12:09:43 +0200

    Prepare phone/email sections

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class App extends StatelessWidget {
                   height: 30,
                 ),
                 Text(
-                  'Rafał Tchórzewski',
+                  'Joe Doe',
                   style: TextStyle(
                     color: Colors.white,
                     fontFamily: 'Pacifico',
@@ -40,6 +40,67 @@ class App extends StatelessWidget {
                     fontSize: 25,
                     fontWeight: FontWeight.w400,
                     letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                  width: 300.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 50.0,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                          '+48 123 456 789',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 2.5,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 50.0,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.email,
+                          color: Colors.teal,
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                          'contact.me@email.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 2.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
